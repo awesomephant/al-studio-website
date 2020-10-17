573fedee86518a62580e23d04e40704563701725
Max Kohler <max@koehler-kn.de>
2020-10-17 14:59:26 +0100

    Develop design

--- js/initLightbox.js.js
+++ js/initLightbox.js.js
@@ -1,11 +1,14 @@
 function initLightbox() {
   const mediaItems = document.querySelectorAll(".project-media-item button");
   const lightbox = document.querySelector(".lightbox-container");
+  const mediaContainer = lightbox.querySelector(".media-container");
+  const captionEl = lightbox.querySelector(".lightbox-caption");
 
   function closeLightbox() {
     document.body.classList.remove("lightbox-active");
     window.setTimeout(function () {
-      lightbox.innerHTML = "";
+      mediaContainer.innerHTML = "";
+      captionEl.innerText = "";
     }, 300);
   }
 
@@ -17,17 +20,14 @@ function initLightbox() {
     item.addEventListener("click", (e) => {
       document.body.classList.add("lightbox-active");
       const mediaEl = item.querySelector("picture");
-      
-      const captionEl = document.createElement("p");
-      captionEl.classList.add("lightbox-caption");
-      captionEl.innerText = mediaEl.getAttribute("data-caption")
-      
+
+      captionEl.innerText = mediaEl.getAttribute("data-caption");
+
       const newMediaEl = document.createElement("img");
       newMediaEl.setAttribute("src", mediaEl.getAttribute("data-full"));
       newMediaEl.classList.add("lightbox-media");
-      
-      lightbox.appendChild(newMediaEl);
-      lightbox.appendChild(captionEl);
+
+      mediaContainer.appendChild(newMediaEl);
     });
   });
   window.addEventListener("keyup", (e) => {

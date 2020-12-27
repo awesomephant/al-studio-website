1fe64224d9050285ee6964a1768844f6bf01b333
Max Kohler <max@koehler-kn.de>
2020-12-27 19:23:57 +0000

    Move slider to a system based on idle time

--- js/initSlider.js
+++ js/initSlider.js
@@ -5,7 +5,9 @@ function initSlider(container) {
     nextSlide: 0,
     prevSlide: 0,
     autoAdvance: true,
+    autoAdvanceInterval: 5000,
   };
+  window.idleTime = 0;
   function advance(increment) {
     slides[state.currentSlide].classList.remove("active");
     state.currentSlide += increment;
@@ -16,46 +18,42 @@ function initSlider(container) {
       state.currentSlide = slides.length - 1;
     }
     container.setAttribute("data-slide", state.currentSlide);
-    if (slides[state.currentSlide].getAttribute("data-light") === "true"){
-      document.body.classList.add("is-light")
+    if (slides[state.currentSlide].getAttribute("data-light") === "true") {
+      document.body.classList.add("is-light");
     } else {
-      document.body.classList.remove("is-light")
+      document.body.classList.remove("is-light");
     }
     slides[state.currentSlide].classList.add("active");
   }
 
   container.addEventListener("click", (e) => {
+    window.idleTime = 0;
     if (e.clientX > window.innerWidth / 2) {
       advance(1);
     } else {
       advance(-1);
     }
-    state.autoAdvance = false;
-    window.setTimeout(() => {
-      state.autoAdvance = true;
-    }, 4000);
   });
+
   window.addEventListener("keyup", (e) => {
+    window.idleTime = 0;
     if (e.key === "ArrowRight") {
       advance(1);
-      state.autoAdvance = false;
-      window.setTimeout(() => {
-        state.autoAdvance = true;
-      }, 4000);
     }
     if (e.key === "ArrowLeft") {
       advance(-1);
-      state.autoAdvance = false;
-      window.setTimeout(() => {
-        state.autoAdvance = true;
-      }, 4000);
     }
   });
   window.setInterval(function () {
-    if (state.autoAdvance === true) {
+    window.idleTime += 1;
+  }, 1000);
+
+  window.setInterval(function () {
+    if (window.idleTime > 5) {
       advance(1);
     }
-  }, 4000);
+  }, state.autoAdvanceInterval);
+
   slides[state.currentSlide].classList.add("active");
   slides[state.currentSlide + 1].classList.add("next");
 }

69a76f8718a072555c20cf7e2f5cbc1635684dd6
Max Kohler <hello@maxkohler.com>
2022-04-25 10:55:49 +0200

    Replace @ncwidgets/reorder widget with default relation widget

--- admin/admin.js
+++ admin/admin.js
@@ -1,7 +1,4 @@
 import CMS from 'netlify-cms-app';
-import { Widget as ReorderWidget } from '@ncwidgets/reorder';
-
-CMS.registerWidget(ReorderWidget)
 CMS.init();
 CMS.registerEditorComponent({
   id: "gallery",
